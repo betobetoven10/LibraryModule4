8f1fbdcd3112273ed7721c7294e62ed239e934f1
Alberto Madueno <alberto_mn@outlook.com>
2022-05-26 16:16:10 -0500

    NavigationLink of main view is completed

--- Library/Views/ContentView.swift
+++ Library/Views/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var model:BooksModel
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView{
+                LazyVStack{
+                    ForEach(model.book){ r in
+                        let beto = String(r.id)
+                        NavigationLink(destination: BookDetail(),
+                                       label: {
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                VStack{
+                                    HStack{
+                                        VStack{
+                                            Text(r.title)
+                                            Text(r.author)
+                                        }
+                                        Spacer()
+                                        Image(systemName: "star.fill")
+                                    }
+                                    Image("cover" + beto)
+                                        .resizable()
+                                        .clipped()
+                                        .aspectRatio(contentMode: .fit)
+                                }
+                            }
+                        })
+                    }
+                }
+                .padding()
+            }
+            .navigationBarTitle("My Library")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(BooksModel())//Adding this line so that when the app launches, it will fetch the data.
     }
 }
---
+++ Library/Views/Reader.swift
@@ -0,0 +1,20 @@
+//
+//  Reader.swift
+//  Library
+//
+//  Created by Alberto Madueño Navarrete on 5/26/22.
+//
+
+import SwiftUI
+
+struct Reader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Reader_Previews: PreviewProvider {
+    static var previews: some View {
+        Reader()
+    }
+}
